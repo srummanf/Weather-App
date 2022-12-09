08c8d7513e502681d03f7882fad5e24a4b9c573d
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2022-12-09 11:24:12 +0530

    Add files via upload

---
+++ Weather App/script.js
@@ -0,0 +1,51 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ca293d1915msh0024a921a01f658p15d96cjsn184a44b680dd',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+const getWeather = (city) => {
+    cityName.innerHTML = city
+    cityName2.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+        .then(response => response.json())
+        .then(response => {
+
+            console.log(response)
+
+            cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+
+            cloud_pct2.innerHTML = response.cloud_pct
+            temp2.innerHTML = response.temp
+            feels_like2.innerHTML = response.feels_like
+            humidity2.innerHTML = response.humidity
+            min_temp2.innerHTML = response.min_temp
+            max_temp2.innerHTML = response.max_temp
+            wind_speed2.innerHTML = response.wind_speed
+            wind_degrees2.innerHTML = response.wind_degrees
+            sunrise2.innerHTML = response.sunrise
+            sunset2.innerHTML = response.sunset
+
+
+        })
+        .catch(err => console.error(err));
+}
+
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather("Kolkata")
